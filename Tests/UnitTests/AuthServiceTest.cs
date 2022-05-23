a133c8300ee8ea76449bb1511ba949669341ad5c
sergey <serg@LAPTOP-EL9802CI>
2022-05-23 13:20:50 +0700

    unit tests

---
+++ Tests/UnitTests/AuthServiceTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.DependencyInjection;
+using Presentation;
+using TemperatureMonitor.Application.Auth;
+using TemperatureMonitor.Application.Auth.Interfaces;
+
+namespace TemperatureMonitor.Tests.UnitTests
+{
+    [TestClass]
+    public class AuthServiceTest
+    {
+        readonly IServiceProvider _services =
+            Program.CreateHostBuilder(new string[] { }).Build().Services;
+
+        [TestInitialize]
+        public void Init()
+        {
+
+        }
+        [TestMethod]
+        public void TestLogin()
+        {
+            var authService = _services.GetRequiredService<IAuthService>();
+        }
+    }
+}
